5ff72c4551efc95f686ba7a35a5e3601f0856851
Parhelion2222 <kurtramel304@gmail.com>
2026-04-26 17:12:07 -1000

    Update RamelFinals_app.py

--- RamelFinals_app.py
+++ RamelFinals_app.py
@@ -42,52 +42,58 @@ df_scatter = px.scatter(df,
 
 st.plotly_chart(df_scatter)
 
-df_pie = df.groupby(["Field_of_Study", ])["Starting_Salary"].mean().reset_index()
+#Side-by-Side
+col1, col2 = st.columns(2)
 
-piechart = px.bar(df_pie, x='Field_of_Study', y='Starting_Salary', title='Starting Salary by Field of Study')
+#Bar Chart
+with col1:
+    df_bar = df.groupby(["Field_of_Study", ])["Starting_Salary"].mean().reset_index()
 
-st.plotly_chart(piechart) 
+    barchart = px.bar(df_pie, x='Field_of_Study', y='Starting_Salary', title='Starting Salary by Field of Study')
 
-
-# Range Slider with Vertically Stacked Subplots
-df_grouped = (
-    df.groupby(["Field_of_Study", "Work_Life_Balance"])["Starting_Salary"]
-    .mean()
-    .reset_index()
-    .sort_values("Work_Life_Balance")
-)
+    st.plotly_chart(barchart) 
 
 
-df_grouped["Work_jitter"] = df_grouped["Work_Life_Balance"] + np.random.uniform(-0.30, 0.30, len(df_grouped))
-
-fig = px.line(
-    df_grouped,
-    x="Work_jitter",  
-    y="Starting_Salary",
-    color="Field_of_Study",
-    markers=True,
-    title="Work-Life Balance vs Starting Salary by Field of Study",
-    labels={
-        "Work_Life_Balance_jitter": "Work-Life Balance",
-        "Starting_Salary": "Avg Starting Salary ($)",
-    },
-)
-
-fig.update_traces(
-    line=dict(width=2),
-    marker=dict(size=8, opacity=0.4),
-    opacity=0.8
-)
-
-fig.update_layout(
-    yaxis=dict(rangemode="tozero"),
-    xaxis=dict(
-        tickmode="linear",
-        dtick=1,
-        rangeslider=dict(visible=True, thickness=0.05),
-    ),
-)
-
-st.plotly_chart(fig, use_container_width=True)
+#Range Slider with Vertically Stacked Subplots
+with col2:
+    df_grouped = (
+        df.groupby(["Field_of_Study", "Work_Life_Balance"])["Starting_Salary"]
+        .mean()
+        .reset_index()
+        .sort_values("Work_Life_Balance")
+    )
+    
+    
+    df_grouped["Work_jitter"] = df_grouped["Work_Life_Balance"] + np.random.uniform(-0.30, 0.30, len(df_grouped))
+    
+    fig = px.line(
+        df_grouped,
+        x="Work_jitter",  
+        y="Starting_Salary",
+        color="Field_of_Study",
+        markers=True,
+        title="Work-Life Balance vs Starting Salary by Field of Study",
+        labels={
+            "Work_Life_Balance_jitter": "Work-Life Balance",
+            "Starting_Salary": "Avg Starting Salary ($)",
+        },
+    )
+    
+    fig.update_traces(
+        line=dict(width=2),
+        marker=dict(size=8, opacity=0.4),
+        opacity=0.8
+    )
+    
+    fig.update_layout(
+        yaxis=dict(rangemode="tozero"),
+        xaxis=dict(
+            tickmode="linear",
+            dtick=1,
+            rangeslider=dict(visible=True, thickness=0.05),
+        ),
+    )
+    
+    st.plotly_chart(fig, use_container_width=True)
 
 
