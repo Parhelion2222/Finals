be6c49a8f92fa916fffc1607803cb85dee7c4fd1
Parhelion2222 <kurtramel304@gmail.com>
2026-05-02 20:57:34 -1000

    Update RamelFinals_app.py

--- RamelFinals_app.py
+++ RamelFinals_app.py
@@ -38,15 +38,16 @@ st.plotly_chart(df_scatter)
 #Side-by-Side
 if "selected_field" not in st.session_state:
     st.session_state.selected_field = None
-    
+
+df_bar = df.groupby("Field_of_Study")["Career_Satisfaction"].mean().reset_index()
+
+    fields = ["All"] + sorted(df["Field_of_Study"].unique().tolist())
+    selected_field = st.selectbox("Filter by Field of Study", fields)
+
 col1, col2, col3 = st.columns(3)
 
 #Bar Chart
 with col1:
-    df_bar = df.groupby("Field_of_Study")["Career_Satisfaction"].mean().reset_index()
-
-    fields = ["All"] + sorted(df["Field_of_Study"].unique().tolist())
-    selected_field = st.selectbox("Filter by Field of Study", fields)
 
     barchart = px.bar(
         df_bar,
