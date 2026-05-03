c3043d4607cc18316fee018a97c3921020b48b60
Parhelion2222 <kurtramel304@gmail.com>
2026-05-02 18:40:01 -1000

    Update RamelFinals_app.py

--- RamelFinals_app.py
+++ RamelFinals_app.py
@@ -132,7 +132,7 @@ fig.update_layout(
 st.plotly_chart(fig, use_container_width=True)
 
 fig = fig = px.area(
-     df.groupby(["Networking_Score", "Projects_Completed"])["Soft_Skills_Score"].mean().reset_index(),
+     df.groupby(["Networking_Score", "Current_Job_Level"])["Projects_Completed"].mean().reset_index(),
     x="Networking_Score",
     y="Projects_Completed",
     color = "Current_Job_Level",
