94bb6d37234d05f52a4ca3095304844a26aa3bba
Parhelion2222 <kurtramel304@gmail.com>
2026-05-02 18:06:50 -1000

    Update RamelFinals_app.py

--- RamelFinals_app.py
+++ RamelFinals_app.py
@@ -131,8 +131,19 @@ fig.update_layout(
 
 st.plotly_chart(fig, use_container_width=True)
 
-scatter_ani = px.scatter(df, x="Career_Satisfaction", y="Work_Life_Balance", animation_frame="Starting_Salary", animation_group="Gender",
-           size="Job_Offers", color="Field_of_Study", hover_name="Gender",
-           log_x=True, size_max=30, range_x=[0,11], range_y=[0,11])
+scatter_ani = px.scatter(
+    df,
+    x="Career_Satisfaction",
+    y="Work_Life_Balance",
+    animation_frame="Starting_Salary",
+    animation_group="Gender",
+    size="Job_Offers",
+    color="Field_of_Study",
+    hover_name="Gender",
+    size_max=30,
+    range_x=[0, 11],
+    range_y=[0, 11],
+    title="Career Satisfaction vs Work-Life Balance over Starting Salary"
+)
 
 st.plotly_chart(scatter_ani, use_container_width=True)
