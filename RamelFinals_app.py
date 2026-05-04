30ed53ad0f817c58840a00a964019da2f1942974
Parhelion2222 <kurtramel304@gmail.com>
2026-05-03 17:00:42 -1000

    Update RamelFinals_app.py

--- RamelFinals_app.py
+++ RamelFinals_app.py
@@ -78,7 +78,7 @@ with col1:
         values="Career_Satisfaction",
         title="Career Satisfaction by Field of Study",
         hole=0.5, 
-        color_discrete_sequence=px.colors.qualitative.viridis,
+        color_continuous_scale=px.colors.sequential.Viridis,
         labels={
         "Field_of_Study": "Field of Study",
         "Career_Satisfaction": "Career Satisfaction"
@@ -131,7 +131,7 @@ with col2:
         df_grouped,
         x="Work_jitter",  
         y="Starting_Salary",
-        color_discrete_sequence=px.colors.qualitative.viridis,
+        color_continuous_scale=px.colors.sequential.Viridis,
         markers=True,
         title="Work-Life Balance by Starting Salary",
         labels={
