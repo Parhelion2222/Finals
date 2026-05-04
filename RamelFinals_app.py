9e139f8c0a49e6a80482efaefcff968aa7f2d4b3
Parhelion2222 <kurtramel304@gmail.com>
2026-05-03 16:06:20 -1000

    Update RamelFinals_app.py

--- RamelFinals_app.py
+++ RamelFinals_app.py
@@ -67,28 +67,28 @@ col1, col2 = st.columns(2)
 #Bar Chart
 with col1:
 
-donut = px.pie(
-    df_bar,
-    names="Field_of_Study",
-    values="Career_Satisfaction",
-    title="Career Satisfaction by Field of Study",
-    hole=0.5,  # ← makes it a donut (0 = full pie, 1 = no chart)
-)
-
-if selected_field != "All":
-    donut.update_traces(
-        opacity=0.3,
-        pull=[0.1 if f == selected_field else 0 for f in df_bar["Field_of_Study"]]  # pulls out selected slice
+    donut = px.pie(
+        df_bar,
+        names="Field_of_Study",
+        values="Career_Satisfaction",
+        title="Career Satisfaction by Field of Study",
+        hole=0.5,  # ← makes it a donut (0 = full pie, 1 = no chart)
     )
-    donut.for_each_trace(
-        lambda t: t.update(opacity=1.0) if t.name == selected_field else None
+    
+    if selected_field != "All":
+        donut.update_traces(
+            opacity=0.3,
+            pull=[0.1 if f == selected_field else 0 for f in df_bar["Field_of_Study"]]  # pulls out selected slice
+        )
+        donut.for_each_trace(
+            lambda t: t.update(opacity=1.0) if t.name == selected_field else None
+        )
+    
+    donut.update_layout(
+        showlegend=True,
+        height=800
     )
-
-donut.update_layout(
-    showlegend=True,
-    height=800
-)
-st.plotly_chart(donut, use_container_width=True)
+    st.plotly_chart(donut, use_container_width=True)
     
 with col2:
 
